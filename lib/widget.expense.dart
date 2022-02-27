a4223ed9004920c2231cce3b8759a8bbba414626
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-02-27 14:50:51 +0530

    signIn page basic degin

--- lib/widget.expense.dart
+++ lib/widget.expense.dart
@@ -4,46 +4,49 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Expense extends StatelessWidget {
-  final date, amount, category,title,subtitle;
+  final date, amount, category, title, subtitle;
 
-  Expense({this.date, this.amount, this.category,this.title,this.subtitle});
+  Expense({this.date, this.amount, this.category, this.title, this.subtitle});
 
   Map categoryIcons = {
     "food": Icon(
-        Icons.fastfood_rounded,
+      Icons.fastfood_rounded,
       color: Colors.white,
     ),
     "travel": Icon(
       Icons.airplanemode_active,
       color: Colors.white,
     ),
-    "shopping":Icon(
+    "shopping": Icon(
       Icons.shopping_cart,
       color: Colors.white,
     )
   };
 
-  List<Widget> categoryIcon = [
-    Icon(
-        Icons.fastfood_rounded,
-      color: Colors.white,
-    ),
-    Icon(
-      Icons.airplanemode_active,
-      color: Colors.white,
-    ),
-    Icon(
-      Icons.shopping_cart,
-      color: Colors.white,
-    )
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Card(
-      child:ListTile(title: Text("${title}"),subtitle: Text("${subtitle}"),
-      leading: categoryIcons[category],trailing:Text("${title}"),
-      selectedTileColor:Colors.cyan,selected:true,
-    ));
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 0,
+        child: ListTile(
+          title: Text(
+            "${date}",
+            style: TextStyle(color: Colors.white),
+          ),
+          subtitle: Text(
+            "${amount}",
+            style: TextStyle(color: Colors.white),
+          ),
+          leading: categoryIcons[category],
+          trailing: Text(
+            "${amount}",
+            style: TextStyle(color: Colors.white),
+          ),
+          selectedTileColor: Color(0xff190933),
+          selected: true,
+
+        ));
   }
 }
