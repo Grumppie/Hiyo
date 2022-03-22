02d3bbd80de36313fe4d7a8d92f893e7b698f6cf
Ashutosh Raj Gupta <ag2364443@gmail.com>
2022-03-22 14:29:36 +0530

    Statistics page created

---
+++ lib/screens/statistics/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hiyo/screens/statistics/create_page.dart';
+//import 'package:pie_chart/pie_chart.dart';
+
+
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:create_page(),
+      //home:DemoApp(),
+    );
+  }
+}
+
+
