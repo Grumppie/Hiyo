19cd129a99ece09adbaaba6557698d3dfcac6760
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-03-22 13:35:37 +0530

    background change

--- lib/screens/create_page.dart
+++ lib/screens/create_page.dart
@@ -13,6 +13,7 @@ class CreatePage extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(appTitle),
+          backgroundColor: Color(0xff303bba),
         ),
         body: MyCustomForm(),
       ),
@@ -58,177 +59,193 @@ class MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            //Date
-            Container(
-              decoration: BoxDecoration(
-                  color: Color(0xff0e4562),
-                  borderRadius: BorderRadius.circular(20)),
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-              margin: EdgeInsets.all(12),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(10, 30, 50, 0),
-                    child: TextFormField(
-                      controller: dateController,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                            )),
-                        icon: const Icon(
-                          Icons.calendar_view_day_rounded,
-                          color: Colors.white,
-                        ),
-                        hintText: 'Enter Date',
-                        hintStyle: TextStyle(color: Colors.white),
-                        labelText: 'Date',
-                        labelStyle: TextStyle(color: Colors.white),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(
-                            color: Colors.white,
-                          ),
-                        ),
+      backgroundColor: Color(0xff1d2473),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //Date
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                  child: Text('Add Expense',
+                      style: TextStyle(color: Colors.white, fontSize: 20))),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xff190933),
+                    borderRadius: BorderRadius.circular(40)),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                margin: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
                       ),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter valid date';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-
-                  //Kharcha
-                  Container(
-                    margin: EdgeInsets.fromLTRB(10, 10, 50, 0),
-                    child: TextFormField(
-                      controller: amountController,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                            borderSide: BorderSide(
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 30, 50, 0),
+                        child: TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          controller: dateController,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                                borderSide: BorderSide(
+                                  color: Color(0xff7b6f6f),
+                                )),
+                            icon: const Icon(
+                              Icons.calendar_today_rounded,
                               color: Colors.white,
-                            )),
-                        icon: const Icon(
-                          Icons.money,
-                          color: Colors.white,
-                        ),
-                        hintText: 'Enter amount',
-                        hintStyle: TextStyle(color: Colors.white),
-                        labelText: 'Amount',
-                        labelStyle: TextStyle(color: Colors.white),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(
-                            color: Colors.white,
+                            ),
+                            hintText: 'Enter Date',
+                            hintStyle: TextStyle(color: Color(0xff7b6f6f)),
+                            labelText: 'Date',
+                            labelStyle: TextStyle(color: Color(0xff7b6f6f)),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Color(0xff7b6f6f),
+                              ),
+                            ),
                           ),
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter valid date';
+                            }
+                            return null;
+                          },
                         ),
                       ),
 
-                      // next 3 lines of code are to open numerical keyboard, and not character keyboard
-                      // so that parsing amount input(text) into num wont throw exception
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly
-                      ],
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter valid amount';
-                        }
-                        return null;
-                      },
-                    ),
+                      //Kharcha
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 10, 50, 0),
+                        child: TextFormField(
+                          controller: amountController,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                                borderSide: BorderSide(
+                                  color: Color(0xff7b6f6f),
+                                )),
+                            icon: const Icon(
+                              Icons.account_balance_wallet_outlined,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Enter amount',
+                            hintStyle: TextStyle(color: Color(0xff7b6f6f)),
+                            labelText: 'Amount',
+                            labelStyle: TextStyle(color: Color(0xff7b6f6f)),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Color(0xff7b6f6f),
+                              ),
+                            ),
+                          ),
+
+                          // next 3 lines of code are to open numerical keyboard, and not character keyboard
+                          // so that parsing amount input(text) into num wont throw exception
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter valid amount';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-
-            const SizedBox(
-              height: 30.0,
-            ),
-            //category
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 0,horizontal: 12),
-              padding: EdgeInsets.fromLTRB(12, 5, 20, 5),
-              decoration: BoxDecoration(
-
-                color: Colors.purple,
-                border: Border.all(
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    hint: Text(
-                      "Select Category",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    dropdownColor: Colors.purple,
-                    isExpanded: true,
-                    value: value,
-                    items: items.map(buildMenuItem).toList(),
-                    onChanged: (value) => setState(
-                      () => this.value = value,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            //Save
-            Container(
-              child: Center(
-                child: TextButton(
-                  child: Text("Save", style: TextStyle(fontSize: 15)),
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.pink),
-                    // margin: EdgeInsets.all(100),
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.symmetric(horizontal: 30,vertical: 15)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      //side: BorderSide(color: Colors.red)
-                    )),
-                  ),
-                  onPressed: () {
-                    // print(dateController.text);
-                    // print(amountController.text);
-
-                    // It returns true if the form is valid, otherwise returns false
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a Snackbar.
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(content: Text('Data is in processing.')));
-
-                      Navigator.of(context, rootNavigator: true).pop(Expense(
-                        date: dateController.text,
-                        amount: amountController.text,
-                        category: value,
-                      ));
-                    }
-                  },
                 ),
               ),
-            ),
-          ],
+
+              const SizedBox(
+                height: 30.0,
+              ),
+              //category
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+                padding: EdgeInsets.fromLTRB(12, 5, 20, 5),
+                height: 72,
+                decoration: BoxDecoration(
+                  color: Color(0xff190933),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      hint: Text(
+                        "Select Category",
+                        style: TextStyle(color: Color(0xff7b6f6f)),
+                      ),
+                      focusColor: Color(0xff000000),
+                      dropdownColor: Color(0xff00a7e1),
+                      borderRadius: BorderRadius.circular(30),
+                      isExpanded: true,
+                      value: value,
+                      items: items.map(buildMenuItem).toList(),
+                      onChanged: (value) => setState(
+                        () => this.value = value,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              //Save
+              Container(
+                child: Center(
+                  child: TextButton(
+                    child: Text("Save", style: TextStyle(fontSize: 15)),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.pink),
+                      // margin: EdgeInsets.all(100),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 15)),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        //side: BorderSide(color: Colors.red)
+                      )),
+                    ),
+                    onPressed: () {
+                      // print(dateController.text);
+                      // print(amountController.text);
+
+                      // It returns true if the form is valid, otherwise returns false
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a Snackbar.
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text('Data is in processing.')));
+
+                        Navigator.of(context, rootNavigator: true).pop(Expense(
+                          date: dateController.text,
+                          amount: amountController.text,
+                          category: value,
+                        ));
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -37,106 +37,112 @@ class _HomeState extends State<Home> {
           },
           child: Icon(Icons.add),
         ),
-        body: Center(
-            child: Container(
-          padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 32.0),
-          child: Column(
-            children: [
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
+        body: SingleChildScrollView(
+          child: Center(
+              child: Container(
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80'),
+                          radius: 30,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Hey!",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white
+                              ),
+                            ),
+                            Text(
+                              "uknown user ",
+                              style: TextStyle(
+                                fontSize: 20,
+                                  color: Colors.white
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color(0xff190933),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  constraints: BoxConstraints(
+                    minWidth: 400,
+                    maxWidth: 400,
+                    minHeight: 180,
+                  ),
+                  child: Column(
                     children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80'),
-                        radius: 30,
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Hey!",
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "My Limit:",
                             style: TextStyle(
-                              fontSize: 30,
-                            ),
-                          ),
-                          Text(
-                            "uknown user ",
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.normal),
+                          )),
+                      Text(
+                        "$goal",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45.0,
+                            height: 1.2,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5,),
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Spent:",
                             style: TextStyle(
-                              fontSize: 20,
-                            ),
-                          )
-                        ],
-                      )
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.normal),
+                          )),
+                      Text(
+                        "$remaining",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45.0,
+                            height: 1.2,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ],
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Color(0xff190933),
+                  ),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                constraints: BoxConstraints(
-                  minWidth: 400,
-                  maxWidth: 400,
-                  minHeight: 180,
+                SizedBox(
+                  height: 12,
                 ),
-                child: Column(
-                  children: [
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "My Limit:",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.normal),
-                        )),
-                    Text(
-                      "$goal",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45.0,
-                          height: 1.2,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5,),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Spent:",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.normal),
-                        )),
-                    Text(
-                      "$remaining",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45.0,
-                          height: 1.2,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Column(
-                children: data.map((e) => e).toList(),
-              )
-              
-            ],
-          ),
-        )),
+                SingleChildScrollView(
+                  child: Column(
+                    children: data.map((e) => e).toList(),
+                  ),
+                )
+                
+              ],
+            ),
+          )),
+        ),
       ),
     );
   }
